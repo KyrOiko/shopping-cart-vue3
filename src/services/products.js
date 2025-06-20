9eb139b038d3e664aa55dd7903a073c55aa64668
kyriakos oikonomou <kyriakos.economou@propulsionanalytics.com>
2025-06-20 18:17:20 +0300

    feat: api products service

---
+++ src/services/products.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { convertCentsToEuros } from "../utils/convert_currency";
+const baseUrl = process.env.VUE_APP_API_URL;
+class ProductsService {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  async getProducts() {
+    try {
+      const response = await axios.get(`${this.baseUrl}/products`);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      return response.data.products.map((product) => ({
+        ...product,
+        price: convertCentsToEuros(product.price),
+      }));
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      throw error;
+    }
+  }
+
+  async checkout() {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      return {
+        success: true,
+        message: "Checkout successful",
+      };
+    } catch (error) {
+      console.error("Error checking out:", error);
+    }
+  }
+}
+
+const productsService = new ProductsService(baseUrl);
+Object.freeze(productsService);
+
+export default productsService;
